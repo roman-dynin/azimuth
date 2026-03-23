196c4c62b3329cd8026279ee6332ecd15a82ade1
Roman Dynin <strftime@yandex.com>
2026-03-23 22:13:51 +0300

    add Tailwind CSS integration, enhance map layout, and improve waypoint marker styles

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import type { LatLng } from 'leaflet'
+
 import type { EnrichedRoute } from '~~/types/api'
 
 import L from 'leaflet'
@@ -8,6 +10,8 @@ import 'leaflet/dist/leaflet.css'
 const { data: routes } = await useFetch<EnrichedRoute[]>('/api/routes')
 
 const { data: spots } = await useFetch('/api/spots')
+
+const clickLatLng = ref<LatLng | null>(null)
 
 const getRandomColor = () => Math.floor(Math.random() * 254)
 
@@ -27,25 +31,26 @@ onMounted(() => {
 
     const polyline = L.polyline(coordinates, {
       color,
-      weight: route.guideline ? 2 : 1,
+      weight: route.guideline ? 3 : 2,
       dashArray: route.guideline ? undefined : [10, 10],
     })
       .bindTooltip(route.title)
       .addTo(map)
 
-    if (routeIndex === 0) {
-      map.fitBounds(polyline.getBounds(), { maxZoom: 18 })
-    }
-
     route.waypoints.forEach((waypoint, waypointIndex) => {
       L.circleMarker(waypoint.latLng, {
         color,
-        fillOpacity: 0.5,
-        radius: 4,
+        weight: 1,
+        fillOpacity: 0.1,
+        radius: 7,
       })
-        .bindPopup(`Точка #${waypointIndex + 1}<br>Курс: ${waypoint.azimuth}°<br>Время: ~ ${Math.round(waypoint.seconds / 60)} мин.<br>${waypoint.latLng}`)
+        .bindTooltip(`Точка #${waypointIndex + 1}<br>Курс: ${waypoint.azimuth}°<br>Время: ~ ${Math.round(waypoint.seconds / 60)} мин.<br>${waypoint.latLng}`)
         .addTo(map)
     })
+
+    if (routeIndex === 0) {
+      map.fitBounds(polyline.getBounds(), { maxZoom: 18 })
+    }
   })
 
   spots.value!.forEach((spot) => {
@@ -60,10 +65,7 @@ onMounted(() => {
   })
 
   map.on('click', (event) => {
-    const latlng = map.mouseEventToLatLng(event.originalEvent)
-
-    // eslint-disable-next-line no-console
-    console.log(latlng)
+    clickLatLng.value = map.mouseEventToLatLng(event.originalEvent)
   })
 })
 
@@ -73,34 +75,37 @@ useHead({
 </script>
 
 <template>
-  <div id="map" />
+  <div class="flex flex-col h-screen">
+    <div class="grow">
+      <div id="map" class="h-full" />
+    </div>
+    <div class="flex justify-between px-2 py-2 bg-black text-gray-500 text-xs">
+      <div>
+        {{ clickLatLng }}
+      </div>
+      <div>
+        🐙 <a href="https://github.com/roman-dynin/azimuth" target="_blank">@roman-dynin</a>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-:root {
-  --emoji-marker-size: 20px;
-}
+<style lang="css">
+@import "tailwindcss";
 
-#map{
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
+:root {
+  --emoji-marker-size: 32px;
 }
 
 .emoji-marker {
-  font-size: var(--emoji-marker-size);
+  font-size: calc(var(--emoji-marker-size) * 0.5);
+  line-height:    var(--emoji-marker-size);
+  text-align: center;
 
-  line-height: var(--emoji-marker-size);
-
-  background: #fff;
-
+  background: #ffffff;
   border-radius: 50%;
 
   width:  var(--emoji-marker-size) !important;
   height: var(--emoji-marker-size) !important;
-
-  padding: 10px;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
 
   devtools: {
     enabled: true,
+  },
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 
   ssr: false,
